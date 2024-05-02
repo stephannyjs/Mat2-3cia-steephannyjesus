5b4f8677920df192ec4e55fb618b5dfeefac142d
stephannyjs <115154320+stephannyjs@users.noreply.github.com>
2024-05-02 17:07:44 -0300

    lk

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ for(let i=0;i <botoes.length;i++){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-           textos[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
